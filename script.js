ed3b8ee89cd4a60653392f655ab04569dbe0274b
Aditya Silwal <silwala@sheridancollege.ca>
2024-05-24 02:11:16 -0400

    add game logic

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("running");
+function getComputerChoice(){
+    let computerChoices = ["rock", "paper", "scissors"]
+    let computerChoice = Math.floor(Math.random() * 3);
+    
+    return computerChoices[computerChoice];
+}
+
+function getHumanChoice(){
+    return prompt("Enter your choice: ").toLowerCase();
+
+}
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(computerChoice, humanChoice){
+        console.log(`player chose: ${humanChoice}\ncomputer chose: ${computerChoice}`)
+        if(computerChoice == humanChoice){
+            console.log("round tied!");
+        }
+        else if (computerChoice == "rock" && humanChoice == "scissors" || computerChoice == "paper" && humanChoice == "rock" || computerChoice == "scissors" && humanChoice == "paper"){
+            console.log("computer wins the round!");
+            computerScore++;
+        }
+        else{
+            console.log("player wins the round!");
+            humanScore++;
+        }
+    }
+
+    for(let i = 0; i < 5; i++){
+        playRound(getComputerChoice(), getHumanChoice());
+    }
+    console.log("computer score: " + computerScore);
+    console.log("player score: " + humanScore);
+    if(humanScore > computerScore){
+        console.log("player won the game!");
+    }
+    else if(computerScore > humanScore){
+        console.log("computer won the game!");
+    }
+    else{
+        console.log("The game ended in tie!");
+    }
+}
+
+playGame();
+
