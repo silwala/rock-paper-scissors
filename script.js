401263b427e9bd2f60e448424c35ea76298307d9
Aditya Silwal <silwala@sheridancollege.ca>
2024-06-03 02:00:55 -0400

    add ui to play a single round

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-function getComputerChoice(){
-    let computerChoices = ["rock", "paper", "scissors"]
-    let computerChoice = Math.floor(Math.random() * 3);
-    
-    return computerChoices[computerChoice];
-}
-
-function getHumanChoice(){
-    return prompt("Enter your choice: ").toLowerCase();
-
-}
-
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
+
+    const playerChoice = document.querySelector("#player-choice")
+
+    function getComputerChoice(){
+        let computerChoices = ["rock", "paper", "scissors"]
+        let computerChoice = Math.floor(Math.random() * 3);
+        
+        return computerChoices[computerChoice];
+    }
+
+    playerChoice.addEventListener("click", (e) => {
+
+        if(e.target.tagName === "DIV") return;
+
+        playRound(getComputerChoice(), e.target.attributes.id.value)
+    })
 
     function playRound(computerChoice, humanChoice){
         console.log(`player chose: ${humanChoice}\ncomputer chose: ${computerChoice}`)
@@ -29,20 +33,20 @@ function playGame(){
         }
     }
 
-    for(let i = 0; i < 5; i++){
-        playRound(getComputerChoice(), getHumanChoice());
-    }
-    console.log("computer score: " + computerScore);
-    console.log("player score: " + humanScore);
-    if(humanScore > computerScore){
-        console.log("player won the game!");
-    }
-    else if(computerScore > humanScore){
-        console.log("computer won the game!");
-    }
-    else{
-        console.log("The game ended in tie!");
-    }
+    // for(let i = 0; i < 5; i++){
+    //     playRound(getComputerChoice(), getHumanChoice());
+    // }
+    // console.log("computer score: " + computerScore);
+    // console.log("player score: " + humanScore);
+    // if(humanScore > computerScore){
+    //     console.log("player won the game!");
+    // }
+    // else if(computerScore > humanScore){
+    //     console.log("computer won the game!");
+    // }
+    // else{
+    //     console.log("The game ended in tie!");
+    // }
 }
 
 playGame();
